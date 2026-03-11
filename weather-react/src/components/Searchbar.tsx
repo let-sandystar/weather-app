3f2ef89eed5f44ab595cf0a4cd48fff0e78e6a29
Sandra Coco <sandrac94@hotmail.com>
2026-03-11 15:43:20 +0100

    added simple tailwind styling to input and button

--- weather-react/src/components/Searchbar.tsx
+++ weather-react/src/components/Searchbar.tsx
@@ -13,16 +13,16 @@ export default function Searchbar({ onSearch }: SearchbarProps) {
         setValue("");
     }
 
-
     return (
-        <div>
+        <div className="flex items-center gap-2">
             <input type="text"
             value={value}
             onChange={(e) => setValue(e.target.value)}
             onKeyDown={(e) => e.key === "Enter" && handleSearch()}
             placeholder="Search for city"
+            className="rounded-md bg-white/5 px-3 py-1.5 mr-2 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-gray-400"
              />
-             <button onClick={handleSearch}>Search</button>
+             <button className="px-3 py-1.5 text-white bg-transparent hover:bg-gray-800 outline-1 outline-gray-500 rounded-md focus:-outline-offset-2 focus:outline-gray-600" onClick={handleSearch}>Search</button>
         </div>
     );
 }
