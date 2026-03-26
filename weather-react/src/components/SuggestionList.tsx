c9a93f0c452c56c7d82be20ad5d0ba9f057ca2fe
Sandra Coco <sandrac94@hotmail.com>
2026-03-26 11:54:23 +0100

    changed key to index instead because api is giving multiples of same city making the key crash due to not being unique

--- weather-react/src/components/SuggestionList.tsx
+++ weather-react/src/components/SuggestionList.tsx
@@ -10,9 +10,9 @@ export default function SuggestionList( {suggestions, onSelect}: SuggestionProps
         <div>
              {suggestions.length > 0 && (
                 <ul className="absolute mt-12 w-full max-w-md bg-white text-black rounded-md shadow-lg z-10">
-                    {suggestions.map((city) => (
+                    {suggestions.map((city, index) => (
                         <li
-                            key={String(city)}
+                            key={index}
                             onClick={() => onSelect(city)}
                             className="px-3 py-2 hover:bg-gray-200 cursor-pointer"
                         >
