b7a21cbff76603347b0b4073fa63f618f0d4fa58
Sandra Coco <sandrac94@hotmail.com>
2026-03-26 10:29:39 +0100

    created a component for the city suggestion list

---
+++ weather-react/src/components/SuggestionList.tsx
@@ -0,0 +1,26 @@
+type SuggestionProps = {
+    suggestions: String[];
+    onSelect: (city: String) => void;
+}
+
+export default function SuggestionList( {suggestions, onSelect}: SuggestionProps) {
+    if (suggestions.length === 0) return null;
+
+    return (
+        <div>
+             {suggestions.length > 0 && (
+                <ul className="absolute mt-12 w-full max-w-md bg-white text-black rounded-md shadow-lg z-10">
+                    {suggestions.map((city) => (
+                        <li
+                            key={String(city)}
+                            onClick={() => onSelect(city)}
+                            className="px-3 py-2 hover:bg-gray-200 cursor-pointer"
+                        >
+                            {city}
+                        </li>
+                    ))}
+                </ul>
+                )}
+        </div>
+    );
+}
