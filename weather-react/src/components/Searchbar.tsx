2641e658297e5f226ab9c29325ce68fcc90918fb
Sandra Coco <sandrac94@hotmail.com>
2026-03-26 11:55:57 +0100

    exchanged the li to point to component instead and added some console logs to see value, handle select for suggestions

--- weather-react/src/components/Searchbar.tsx
+++ weather-react/src/components/Searchbar.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { fetchCitySuggestions } from "../lib/geodb";
+import SuggestionList from "./SuggestionList";
 
 type SearchbarProps = {
     onSearch: (city: string) => void;
@@ -15,7 +16,7 @@ export default function Searchbar({ onSearch }: SearchbarProps) {
                 setSuggestions([]);
                 return;
             }
-
+            console.log("Debounced value", value);
             const results = await fetchCitySuggestions(value);
             setSuggestions(results);
         }, 300);
@@ -28,33 +29,32 @@ export default function Searchbar({ onSearch }: SearchbarProps) {
         setValue("");
     }
 
+    const handleSelect = (city: String) => {
+        onSearch(String(city));
+        setValue(String(city));
+        setSuggestions([]);
+        console.log("selected city", city);
+    }
+
     return (
-        <div className="relative flex items-center gap-2">
-            <input type="text"
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-            onKeyDown={(e) => e.key === "Enter" && handleSearch()}
-            placeholder="Search for city"
-            className="rounded-md bg-white/5 px-3 py-1.5 mr-2 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-gray-400"
-             />
-             <button className="px-3 py-1.5 text-white bg-transparent hover:bg-gray-800 outline-1 outline-gray-500 rounded-md focus:-outline-offset-2 focus:outline-gray-600" onClick={handleSearch}>Search</button>
-             {suggestions.length > 0 && (
-                <ul className="absolute mt-12 w-full max-w-md bg-white text-black rounded-md shadow-lg z-10">
-                    {suggestions.map((city) => (
-                        <li
-                            key={city}
-                            onClick={() => {
-                            onSearch(city);
-                            setValue("");
-                            setSuggestions([]);
-                            }}
-                            className="px-3 py-2 hover:bg-gray-200 cursor-pointer"
-                        >
-                            {city}
-                        </li>
-                    ))}
-                </ul>
-                )}
+        <div className="relative flex flex-col w-full max-w-md">
+            <div className="flex items-center gap-2">
+                <input type="text"
+                value={value}
+                onChange={(e) => {
+                    setValue(e.target.value);
+                    console.log("Input value", e.target.value);
+                }}
+                onKeyDown={(e) => e.key === "Enter" && handleSearch()}
+                placeholder="Search for city"
+                className="rounded-md bg-white/5 px-3 py-1.5 mr-2 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-gray-400"
+                />
+                <button className="px-3 py-1.5 text-white bg-transparent hover:bg-gray-800 outline-1 outline-gray-500 rounded-md focus:-outline-offset-2 focus:outline-gray-600" onClick={handleSearch}>Search</button>
+            </div>
+        <SuggestionList
+            suggestions={suggestions}
+            onSelect={handleSelect}
+        />
         </div>
     );
 }
