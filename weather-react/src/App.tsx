295d9cac7358795bba068a4376fdc9b0992bb59c
Sandra Coco <sandrac94@hotmail.com>
2026-04-08 08:47:08 +0200

    added text to app to make sure value from searchbar is correct

--- weather-react/src/App.tsx
+++ weather-react/src/App.tsx
@@ -3,11 +3,14 @@ import './App.css'
 import Searchbar from './components/Searchbar'
 
 function App() {
-  const [city, setCity] = useState("Halmstad");
+  const [city, setCity] = useState("");
 
   return (
     <>
       <Searchbar onSearch={setCity} />
+      <div className="mt-6 text-center text-white text-2xl">
+        Vald stad: {city}
+      </div>
     </>
   )
 }
