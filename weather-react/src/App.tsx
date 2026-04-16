d61767e24e7df951a621482ab29ffebaca6fdf6c
Sandra Coco <sandrac94@hotmail.com>
2026-04-16 09:51:27 +0200

    removed app.css it is not needed. Also added some wrappers in app.tsx to center and size elements

--- weather-react/src/App.tsx
+++ weather-react/src/App.tsx
@@ -1,18 +1,18 @@
 import { useState } from 'react'
-import './App.css'
 import Searchbar from './components/Searchbar'
 import CurrentWeatherCard from './components/CurrentWeather';
+import "./index.css";
 
 function App() {
   const [city, setCity] = useState("");
 
   return (
-    <>
-      <Searchbar onSearch={setCity} />
-      <div>
-        <CurrentWeatherCard city = {city}></CurrentWeatherCard>
+    <div className="min-h-screen w-full atmospheric-bg flex flex-col items-center p-6">
+      <div className="w-full max-w-md">
+        <Searchbar onSearch={setCity} />
+        <CurrentWeatherCard city = {city}/>
       </div>
-    </>
+    </div>
   )
 }
 
